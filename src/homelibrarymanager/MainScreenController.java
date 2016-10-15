594f386fc0c684f715e2fec38bdb65f74ba630f6
Ivan <Ivan@IvanLaptop.home>
2016-10-15 19:39:19 -0400

    Created basic main screen layout

--- src/homelibrarymanager/MainScreenController.java
+++ src/homelibrarymanager/MainScreenController.java
@@ -10,7 +10,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -18,10 +22,28 @@ import javafx.scene.control.Label;
  */
 public class MainScreenController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private ComboBox<?> CB_MediaType;
+    @FXML
+    private Button BT_AddMedia;
+    @FXML
+    private TextField TF_MediaTitle;
+    @FXML
+    private Button BT_ManageUsers;
+    @FXML
+    private Button BT_AddUser;
+    @FXML
+    private Button BT_EditUserInfo;
+    @FXML
+    private Label LB_Sort;
+    @FXML
+    private Label LB_Search;
+    @FXML
+    private Label LB_UserGreeting;
+    @FXML
+    private Button BT_UserLogOut;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -31,5 +53,25 @@ public class MainScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void HandleBT_AddMediaClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void HandleBT_ManageUsersClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void HandleBT_AddUserClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void HandleBT_EditUserInfoClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void HandleBT_UserLogOut(MouseEvent event) {
+    }
     
 }
