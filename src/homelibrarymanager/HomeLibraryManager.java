e72972970c3c5b0c5404c8ca9a0baa16c81f5017
Ivan <Ivan@IvanLaptop.home>
2016-10-15 20:46:20 -0400

    Updated Login page

--- src/homelibrarymanager/HomeLibraryManager.java
+++ src/homelibrarymanager/HomeLibraryManager.java
@@ -19,7 +19,7 @@ public class HomeLibraryManager extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
         
         Scene scene = new Scene(root);
         
--- src/homelibrarymanager/LoginScreenController.java
+++ src/homelibrarymanager/LoginScreenController.java
@@ -7,7 +7,13 @@ package homelibrarymanager;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -15,6 +21,18 @@ import javafx.fxml.Initializable;
  * @author Ivan
  */
 public class LoginScreenController implements Initializable {
+    @FXML
+    private TextField TF_Username;
+    @FXML
+    private PasswordField TF_Password;
+    @FXML
+    private Label LB_LoginError;
+    @FXML
+    private Button BT_Login;
+    @FXML
+    private Button BT_NewUser;
+    @FXML
+    private Button BT_ForgotPassword;
 
     /**
      * Initializes the controller class.
@@ -23,5 +41,17 @@ public class LoginScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void HandleBT_loginClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void HandleBT_NewUserClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void HandleBT_ForgotPassword(MouseEvent event) {
+    }
     
 }
