cf1adcb009bc33742e12a92f3837dd0dbd5b2499
Ivan <Ivan@IvanLaptop.home>
2016-10-16 01:45:49 -0400

    Added DB classes

---
+++ src/dbClasses/AppUserJpaController.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbClasses;
+
+import dbClasses.exceptions.NonexistentEntityException;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author Ivan
+ */
+public class AppUserJpaController implements Serializable {
+
+    public AppUserJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(AppUser appUser) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(appUser);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(AppUser appUser) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            appUser = em.merge(appUser);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = appUser.getId();
+                if (findAppUser(id) == null) {
+                    throw new NonexistentEntityException("The appUser with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Integer id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            AppUser appUser;
+            try {
+                appUser = em.getReference(AppUser.class, id);
+                appUser.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The appUser with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(appUser);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<AppUser> findAppUserEntities() {
+        return findAppUserEntities(true, -1, -1);
+    }
+
+    public List<AppUser> findAppUserEntities(int maxResults, int firstResult) {
+        return findAppUserEntities(false, maxResults, firstResult);
+    }
+
+    private List<AppUser> findAppUserEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(AppUser.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public AppUser findAppUser(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(AppUser.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getAppUserCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<AppUser> rt = cq.from(AppUser.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
---
+++ src/dbClasses/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package dbClasses.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
