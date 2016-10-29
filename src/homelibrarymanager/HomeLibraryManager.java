f5730fa91d002e2a9255a62c9a4265e3e4d11885
Ivan <Ivan@IvanLaptop.home>
2016-10-29 01:44:35 -0400

    Refactored main controller classes to remove code repetition

--- src/homelibrarymanager/HomeLibraryManager.java
+++ src/homelibrarymanager/HomeLibraryManager.java
@@ -5,6 +5,7 @@
  */
 package homelibrarymanager;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -30,33 +31,25 @@ public class HomeLibraryManager extends Application {
     }
     
     public void gotoMainScreen(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
-        Scene scene = new Scene(root);
-        stage.setTitle("Home Library Manager");
-        stage.setScene(scene);
-        stage.show();
+        setupScene(stage, "MainScreen.fxml", "Home Library Manager");
     }
     
     public void gotoLoginScreen(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
-        Scene scene = new Scene(root);
-        stage.setTitle("Login to Home Library Manager");
-        stage.setScene(scene);
-        stage.show();
+        setupScene(stage, "LoginScreen.fxml", "Login to Home Library Manager");
     }
     
     public void gotoUserScreen(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("UserScreen.fxml"));
-        Scene scene = new Scene(root);
-        stage.setTitle("Home Library Manager User");
-        stage.setScene(scene);
-        stage.show();
+        setupScene(stage, "UserScreen.fxml", "Home Library Manager User");
     }
     
     public void gotoMediaScreen(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MediaScreen.fxml"));
+        setupScene(stage, "MediaScreen.fxml", "Home Library Manager Media");
+    }
+    
+    private void setupScene(Stage stage, String Resource, String Title) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource(Resource));
         Scene scene = new Scene(root);
-        stage.setTitle("Home Library Manager Media");
+        stage.setTitle(Title);
         stage.setScene(scene);
         stage.show();
     }
