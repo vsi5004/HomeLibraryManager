d329ba3aa0b5cddbef6f5cdc70a12101407f69c4
Ivan <Ivan@IvanLaptop.home>
2016-10-29 02:16:53 -0400

    Refactored Main class goto methods in order to allow for passing of parameters to controllers. Will be useful for Media and Collection pages.

--- src/homelibrarymanager/HomeLibraryManager.java
+++ src/homelibrarymanager/HomeLibraryManager.java
@@ -11,6 +11,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ui.controllers.LoginScreenController;
+import ui.controllers.MainScreenController;
+import ui.controllers.MediaScreenController;
+import ui.controllers.UserScreenController;
 
 /**
  *
@@ -31,23 +35,31 @@ public class HomeLibraryManager extends Application {
     }
     
     public void gotoMainScreen(Stage stage) throws Exception{
-        setupScene(stage, "MainScreen.fxml", "Home Library Manager");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));
+        MainScreenController controller = (MainScreenController) loader.getController();
+        setupScene(stage, "Home Library Manager", loader);
     }
     
     public void gotoLoginScreen(Stage stage) throws Exception{
-        setupScene(stage, "LoginScreen.fxml", "Login to Home Library Manager");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginScreen.fxml"));
+        LoginScreenController controller = (LoginScreenController) loader.getController();
+        setupScene(stage, "Login to Home Library Manager", loader);
     }
     
     public void gotoUserScreen(Stage stage) throws Exception{
-        setupScene(stage, "UserScreen.fxml", "Home Library Manager User");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("UserScreen.fxml"));
+        UserScreenController controller = (UserScreenController) loader.getController();
+        setupScene(stage, "Home Library Manager User", loader);
     }
     
     public void gotoMediaScreen(Stage stage) throws Exception{
-        setupScene(stage, "MediaScreen.fxml", "Home Library Manager Media");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MediaScreen.fxml"));
+        MediaScreenController controller = (MediaScreenController) loader.getController();
+        setupScene(stage, "Home Library Manager Media", loader);
     }
     
-    private void setupScene(Stage stage, String Resource, String Title) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource(Resource));
+    private void setupScene(Stage stage, String Title, FXMLLoader loader) throws IOException{
+        Parent root = (Parent) loader.load();
         Scene scene = new Scene(root);
         stage.setTitle(Title);
         stage.setScene(scene);
