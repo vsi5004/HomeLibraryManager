c130f8d5d5feaa827dc5fd694656e5458736752e
Ivan <Ivan@IvanLaptop.home>
2016-10-29 01:35:31 -0400

    Added Password retrieve functionality

--- src/ui/controllers/LoginScreenController.java
+++ src/ui/controllers/LoginScreenController.java
@@ -52,6 +52,8 @@ public class LoginScreenController implements Initializable {
     @FXML
     private Label LB_QuestionError;
     @FXML
+    private Label LB_SecurityQuestion;
+    @FXML
     private Button BT_GetPassword;
     @FXML
     private Button BT_BackToLogin;
@@ -61,29 +63,25 @@ public class LoginScreenController implements Initializable {
     private Label LB_Password;
 
     HomeLibraryManager manager = new HomeLibraryManager();
+    private AppUser user = new AppUser();
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
     }
 
     @FXML
     private void HandleBT_loginClicked(MouseEvent event) throws Exception {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HomeLibraryManagerPU");
-        AppUserJpaController jpaUser = new AppUserJpaController(emf);
-        List<AppUser> users = jpaUser.findAppUserEntities();
+        if (getAppUser(TF_Username.getText()) && Objects.equals(TF_Password.getText(), user.getUserPassword())) {
 
-        for (AppUser p : users) {
-            if (Objects.equals(TF_Username.getText(), p.getUsername()) && Objects.equals(TF_Password.getText(), p.getUserPassword())) {
-                Stage stage = (Stage) BT_Login.getScene().getWindow();
-                LoggedInUser.getInstance();
-                LoggedInUser.setInfo(p.getUserId(), p.getUsername(), p.getUserType());
-                manager.gotoMainScreen(stage);
-                break;
-            }
+            Stage stage = (Stage) BT_Login.getScene().getWindow();
+            LoggedInUser.getInstance();
+            LoggedInUser.setInfo(user.getUserId(), user.getUsername(), user.getUserType());
+            manager.gotoMainScreen(stage);
+
         }
         TF_Password.clear();
         LB_LoginError.setText("Your Username or Password doesn't match!");
@@ -99,23 +97,64 @@ public class LoginScreenController implements Initializable {
 
     @FXML
     private void HandleBT_ForgotPasswordClicked(MouseEvent event) throws IOException {
-        Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/homelibrarymanager/LoginQuestionScreen.fxml"));
-        stage.setScene(new Scene(root));
-        stage.setTitle("Password Retrieval");
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.initOwner(BT_ForgotPassword.getScene().getWindow());
-        stage.showAndWait();
+        if (getAppUser(TF_Username.getText())) {
+
+            LB_LoginError.setText("");
+
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/homelibrarymanager/LoginQuestionScreen.fxml"));
+            Parent root = (Parent) loader.load();
+            LoginScreenController controller = (LoginScreenController) loader.getController();
+
+            stage.setScene(new Scene(root));
+            stage.setTitle("Password Retrieval");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initOwner(BT_ForgotPassword.getScene().getWindow());
+            controller.setPasswordRetrievalScreen(stage, user); // or what you want to do
+            stage.showAndWait();
+        }
+        else {
+            TF_Username.clear();
+            TF_Password.clear();
+            LB_LoginError.setText("Your Username wasn't found in the database!");
+        }
     }
 
     @FXML
     private void HandleBT_GetPasswordClicked(MouseEvent event) {
+        if(Objects.equals(TF_SecurityAnswer.getText().toUpperCase(),user.getSecurityAnswer().toUpperCase())){
+            LB_Password.setText(user.getUserPassword());
+        }
+        else{
+            LB_Password.setText("");
+            TF_SecurityAnswer.setText("");
+            LB_QuestionError.setText("Your answer is incorrect!");
+        }
     }
 
     @FXML
     private void HandleBT_BackToLoginClicked(MouseEvent event) {
-        Stage stage=(Stage)BT_BackToLogin.getScene().getWindow();
+        Stage stage = (Stage) BT_BackToLogin.getScene().getWindow();
         stage.close();
+    }
+
+    private void setPasswordRetrievalScreen(Stage stage, AppUser u) {
+        user = u;
+        LB_SecurityQuestion.setText(user.getSecurityQuestion());
+    }
+
+    private boolean getAppUser(String Username) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HomeLibraryManagerPU");
+        AppUserJpaController jpaUser = new AppUserJpaController(emf);
+        List<AppUser> users = jpaUser.findAppUserEntities();
+
+        for (AppUser p : users) {
+            if (Objects.equals(Username, p.getUsername())) {
+                user = p;
+                return true;
+            }
+        }
+        return false;
     }
 
 }
