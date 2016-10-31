8d2d6a46150abc9cf6d848be46ce9ca8f7e8b087
Ivan <Ivan@IvanLaptop.home>
2016-10-30 22:50:22 -0400

    Adding DB functionality to Literature page

--- src/ui/controllers/LiteratureScreenController.java
+++ src/ui/controllers/LiteratureScreenController.java
@@ -5,9 +5,16 @@
  */
 package ui.controllers;
 
+import Enums.MediaType;
+import dbClasses.AppMedia;
+import dbClasses.AppMediaJpaController;
+import dbClasses.AppUser;
+import dbClasses.AppUserJpaController;
 import homelibrarymanager.HomeLibraryManager;
 import homelibrarymanager.LoggedInUser;
 import java.net.URL;
+import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,6 +24,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * FXML Controller class
@@ -56,6 +65,8 @@ public class LiteratureScreenController implements Initializable {
     private TextField TF_Edition;
     @FXML
     private Label LB_ValidationMessage;
+    
+    private AppMedia editedMedia = new AppMedia();
 
     /**
      * Initializes the controller class.
@@ -66,7 +77,41 @@ public class LiteratureScreenController implements Initializable {
     }
 
     @FXML
-    private void HandleBT_SaveMediaClicked(MouseEvent event) {
+    private void HandleBT_SaveMediaClicked(MouseEvent event) throws Exception {
+        if (ValidateFields()) {
+            LB_ValidationMessage.setText("Storing in database, please wait.");
+
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("HomeLibraryManagerPU");
+            AppMediaJpaController jpaMedia = new AppMediaJpaController(emf);
+
+            if (editedMedia.getMediaId() != null) {
+                AppMedia media = jpaMedia.findAppMedia(editedMedia.getMediaId());
+                //user.setUsername(TF_UserName.getText());
+                //user.setUserPassword(TF_UserPassword.getText());
+                //user.setSecurityQuestion(TA_SecurityQuestion.getText());
+                //user.setSecurityAnswer(TF_SecurityQuestionAnswer.getText());
+                jpaMedia.edit(media);
+
+            } else {
+                AppMedia media = new AppMedia();
+                media.setType(MediaType.LITERATURE.getValue());
+                media.setFormat(CB_Format.getValue().toString());
+                media.setTitle(TF_Title.getText());
+                media.setAuthor(TF_Author.getText());
+                media.setVolume(TF_Volume.getText());
+                media.setPublisher(TF_Publisher.getText());
+                media.setVersion(TF_Version.getText());
+                media.setEdition(TF_Edition.getText());
+                media.setGenre(TF_Genre.getText());
+                media.setLocation(TF_Location.getText());
+                media.setRating(Integer.parseInt(TF_Rating.getText()));
+                
+                
+                
+                jpaMedia.create(media);
+            }
+            gotoLastPage();
+        }
     }
 
     @FXML
@@ -79,5 +124,34 @@ public class LiteratureScreenController implements Initializable {
         LB_ValidationMessage.setText("");
         LoggedInUser.setLastPage("Literature");
         manager.gotoMainScreen(stage);
+    }
+
+    private boolean ValidateFields() {
+        if (TF_Title.getText().isEmpty() || TF_Author.getText().isEmpty() || CB_Format.getValue() == null) {
+            LB_ValidationMessage.setText("Please fill in Title, Author, and Format fields!");
+            return false;
+        }
+        if (MediaExists(TF_Title.getText(), TF_Author.getText()) && !LoggedInUser.getEditCurrent()) {
+            LB_ValidationMessage.setText("Media item already exists!");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean MediaExists(String Title, String Author) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HomeLibraryManagerPU");
+        AppMediaJpaController jpaMedia = new AppMediaJpaController(emf);
+        List<AppMedia> media = jpaMedia.findAppMediaEntities();
+
+        for (AppMedia m : media) {
+            if (Objects.equals(Title, m.getTitle()) && Objects.equals(Author, m.getAuthor())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void initEditMedia(AppMedia media){
+        editedMedia = media;
     }
 }
